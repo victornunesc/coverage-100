eaa668de6f0b9335430834534745a0a7792db49a
Victor Nunes <victornunesc95@gmail.com>
2022-07-13 18:44:47 -0300

    feat: login and get newest user route

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from accounts.views import AccountView
+from accounts.views import AccountNewestView, AccountView, LoginView
 
-urlpatterns = [path("accounts/", AccountView.as_view())]
+urlpatterns = [
+    path("accounts/", AccountView.as_view()),
+    path("login/", LoginView.as_view()),
+    path("accounts/newest/<int:num>", AccountNewestView.as_view()),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from rest_framework import generics, permissions, authentication
-from rest_framework.views import APIView, Response, Request
+from rest_framework.views import APIView, Response, Request, status
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 
 from accounts.models import Account
-from accounts.serializers import AccountSerializer
+from accounts.serializers import AccountSerializer, LoginSerializer
 
 
 class AccountView(generics.ListCreateAPIView):
@@ -12,8 +13,29 @@ class AccountView(generics.ListCreateAPIView):
 
 
 class AccountNewestView(generics.ListAPIView):
-    ...
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def get_queryset(self):
+        num = self.kwargs["num"]
+        return self.queryset.order_by("-date_joined")[0:num]
 
 
 class LoginView(APIView):
-    ...
+    def post(self, request: Request):
+        serialized = LoginSerializer(data=request.data)
+        serialized.is_valid(raise_exception=True)
+
+        user = authenticate(**serialized.data)
+
+        print(user)
+
+        if not user:
+            return Response(
+                {"detail": "invalid email or password"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({"Token": token.key}, status=status.HTTP_200_OK)
