6668c2feab8270517d302589f2532ae68cc9855d
Victor Nunes <victornunesc95@gmail.com>
2022-07-17 16:25:03 -0300

    test: AccountIdView

--- accounts/tests/tests_views.py
+++ accounts/tests/tests_views.py
@@ -1,5 +1,5 @@
 from rest_framework.test import APITestCase
-
+from rest_framework.authtoken.models import Token
 from accounts.models import Account
 from faker import Faker
 
@@ -48,9 +48,62 @@ class AccountNewestViewTest(APITestCase):
 
     def test_newest_accounts(self):
         response = self.client.get(self.url)
-        print(response.data)
 
         self.assertEqual(response.status_code, 200)
         self.assertTrue(
             response.data[0]["date_joined"] > response.data[1]["date_joined"]
+        )
+
+
+class AccountIdView(APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.url = "/api/accounts/1/"
+        cls.account_data = {
+            "email": "test@mail.com",
+            "password": "1234",
+            "first_name": "John",
+            "last_name": "Doe",
+            "is_seller": True,
+        }
+
+    def setUp(self) -> None:
+        self.first_name = fake.first_name()
+        self.account = Account.objects.create_user(**self.account_data)
+
+    def test_patch_account_with_credentials(self):
+        token = Token.objects.create(user=self.account)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
+
+        response = self.client.patch(self.url, {"first_name": self.first_name})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.data["first_name"] == self.first_name)
+
+    def test_fail_patch_account_without_credentials(self):
+        response = self.client.patch(self.url, {"first_name": self.first_name})
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json(),
+            {"detail": "Authentication credentials were not provided."},
+        )
+
+    def test_fail_patch_account_with_insufficient_credentials(self):
+        other_account = Account.objects.create_user(
+            email="test2@mail.com",
+            password="1234",
+            is_seller=False,
+        )
+
+        token = Token.objects.create(user=other_account)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
+
+        response = self.client.patch(self.url, {"first_name": self.first_name})
+
+        print(response.data)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            response.json(),
+            {"detail": "You do not have permission to perform this action."},
         )
